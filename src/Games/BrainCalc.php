e8b3ca45ffb94ff08c44d3b5f784deb2469a3387
NikolayIz <nikolay97altov@gmail.com>
2025-04-18 22:19:19 +0300

    fix BrainCalc diff functions

--- src/Games/BrainCalc.php
+++ src/Games/BrainCalc.php
@@ -11,46 +11,48 @@ function getDescriptionGameCalc(): string
     return 'What is the result of the expression?';
 }
 
-function generateQuestionGameCalc(): string
+function calculateExpression(int $num1, string $operator, int $num2): ?int
 {
-    $numA = random_int(1, 10);
-    $numB = random_int(1, 10);
-    $operatorNum = random_int(1, 3);
-    $operator = ' ';
-    switch ($operatorNum) {
-        case 1:
-            $operator = '+';
-            break;
-        case 2:
-            $operator = '-';
-            break;
-        case 3:
-            $operator = '*';
-            break;
-    }
-    return "{$numA} {$operator} {$numB}";
-}
-
-function getRightAnswerGameCalc(string $question): string
-{
-    $arrayStr = explode(' ', $question);
-    $numA = (int)$arrayStr[0];
-    $oper = $arrayStr[1];
-    $numB = (int)$arrayStr[2];
-    switch ($oper) {
+    switch ($operator) {
         case "+":
-            $result = $numA + $numB;
+            return $num1 + $num2;
             break;
         case "-":
-            $result = $numA - $numB;
+            return $num1 - $num2;
             break;
         case "*":
-            $result = $numA * $numB;
+            return $num1 * $num2;
             break;
         default:
-            $result = null;
+            return null;
     }
-    return (string)$result; //тип string потому что ответ пользователя принимается в string
+}
+
+function generateQuestionGameCalc(): array
+{
+    $num1 = random_int(1, 10);
+    $num2 = random_int(1, 10);
+    $operatorNum = random_int(1, 3);
+    $operator = match ($operatorNum) {
+        1 => '+',
+        2 => '-',
+        3 => '*',
+    };
+    $result = ['num1' => $num1, 'num2' => $num2, 'operator' => $operator];
+    return $result;
+}
+
+function getRightAnswerGameCalc(array $question): string
+{
+    $num1 = $question['num1'];
+    $operator = $question['operator'];
+    $num2 = $question['num2'];
+    return (string)calculateExpression($num1, $operator, $num2); // правильный ответ обязательно в string
+}
+
+function getQuestionTextGameCalc(array $question): string
+{
+    return "{$question['num1']} {$question['operator']} {$question['num2']}";
 }
 
 function launchCalcGame(): void
@@ -58,6 +60,7 @@ function launchCalcGame(): void
     launchEngineGame(
         fn() => getDescriptionGameCalc(),
         fn() => generateQuestionGameCalc(),
-        fn(string $question) => getRightAnswerGameCalc($question)
+        fn(array $question) => getRightAnswerGameCalc($question),
+        fn(array $question) => getQuestionTextGameCalc($question)
     );
 }
