5a9d622944b5a11fca7ad06eb7e744e0ab607b25
NikolayIz <nikolay97altov@gmail.com>
2025-04-18 22:40:50 +0300

    fix BrainGcd diff functions

--- src/Games/BrainGcd.php
+++ src/Games/BrainGcd.php
@@ -11,18 +11,17 @@ function getDescriptionGameGcd(): string
     return 'Find the greatest common divisor of given numbers.';
 }
 
-function generateQuestionGameGcd(): string
+function generateQuestionGameGcd(): array
 {
-    $numA = random_int(1, 100);
-    $numB = random_int(1, 100);
-    return "{$numA} {$numB}";
+    $num1 = random_int(1, 100);
+    $num2 = random_int(1, 100);
+    return ['num1' => $num1, 'num2' => $num2];
 }
 
-function getRightAnswerGameGcd(string $question): string
+function getRightAnswerGameGcd(array $question): string
 {
-    $arrayStr = explode(' ', $question);
-    $a = (int)$arrayStr[0];
-    $b = (int)$arrayStr[1];
+    $a = $question['num1'];
+    $b = $question['num2'];
     $temp = 0; //промежуточная переменная
     //алгоритм Евклида by Nikolay Gagarinov:
     $result = $a;
@@ -35,11 +34,17 @@ function getRightAnswerGameGcd(string $question): string
     return (string)$result; //тип string потому что ответ пользователя принимается в string
 }
 
+function getQuestionTextGameGcd(array $question): string
+{
+    return "{$question['num1']} {$question['num2']}";
+}
+
 function launchGcdGame(): void
 {
     launchEngineGame(
         fn() => getDescriptionGameGcd(),
         fn() => generateQuestionGameGcd(),
-        fn(string $question) => getRightAnswerGameGcd($question)
+        fn(array $question) => getRightAnswerGameGcd($question),
+        fn(array $question) => getQuestionTextGameGcd($question)
     );
 }
